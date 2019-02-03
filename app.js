288e8d96a15ca7dd7d45898eb0cf87353db92d2f
himanshu <hjhimanshu01@gmail.com>
2019-02-02 23:21:52 -0500

    styling left

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const bodyParser = require('body-parser')
+const path = require('path')
+const port = process.env.PORT || 3000 ;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}))
+// app.use(express.static(path.join(__dirname,'index')))
+app.set('views',path.join(__dirname,'views'));
+app.set("view engine","jade");
+app.use(express.static('./'))
+
+
+app.get('/',(req,res)=>{
+    
+    res.render('index');
+})
+
+io.sockets.on('connection', (socket) => {
+    socket.on('mousemove', (data) => {
+      socket.broadcast.emit('moving', data);
+    });
+  });
+  io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
+
+server.listen(port, function(err) {
+    console.log(port)
+    if(!err) { console.log("Listening on port " + port); }
+});
